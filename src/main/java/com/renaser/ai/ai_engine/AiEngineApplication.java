5a3defb7e80283f72d75b63689f12518aeae5add
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-08-13 17:28:14 -0500

    estructura de agentes de ai

---
+++ src/main/java/com/renaser/ai/ai_engine/AiEngineApplication.java
@@ -0,0 +1,13 @@
+package com.renaser.ai.ai_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renaser/ai/ai_engine/AiEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renaser.ai.ai_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
